8e1e61f82cd1d8066bc202c3397d09ef75360012
marcodesparza <esparza231093@hotmail.com>
2024-06-08 15:30:07 -0600

    add shadows to menu description

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 `
 
 document.querySelector<HTMLDivElement>('#press')!.innerHTML = `
-  <div class="toolbox" id=caja>
+  <div id=caja>
   <img src="${viteLogo}" class="logo" alt="Vite logo" />
   </div>
   `
@@ -30,9 +30,9 @@ document.querySelector<HTMLDivElement>('#press')!.innerHTML = `
 
   document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h2 class="notes" id=el1>Quien Soy</h2>
-    <h2 class="notes" id=el2>Experiencia</h2>
-    <h2 class="notes" id=el3>Sueños</h2>
+    <h2 class="notes" id="el1">Quien Soy</h2>
+    <h2 class="notes" id="el2">Experiencia</h2>
+    <h2 class="notes" id="el3">Sueños</h2>
     </div>
   </div>
   `
@@ -63,12 +63,21 @@ onmouse.forEach(el => {
     switch (test2){
       case "el1":
         document.getElementById('caja').innerHTML = "Soy Marco"
+        document.getElementById('el2').style.opacity = "0.1"
+        document.getElementById('el3').style.opacity = "0.1"
+        document.getElementById('el1').style.opacity = "1"
         break
       case "el2":
         document.getElementById('caja').innerHTML = "Product Owner, SysAdmin"
+        document.getElementById('el1').style.opacity = "0.1"
+        document.getElementById('el3').style.opacity = "0.1"
+        document.getElementById('el2').style.opacity = "1"
         break
       case "el3":
-        document.getElementById('caja').innerHTML = "vivir en maza"
+        document.getElementById('caja').innerHTML = "En la mayoría de los casos permite que el elemento hijo tome su ancho especificado, y el navegador distribuye el espacio restante de forma equitativa entre el par de márgenes izquierdo y derecho o el par de márgenes superior e inferior o entre ambos pares."
+        document.getElementById('el3').style.opacity = "1"
+        document.getElementById('el2').style.opacity = "0.1"
+        document.getElementById('el1').style.opacity = "0.1"
         break
     }
   })

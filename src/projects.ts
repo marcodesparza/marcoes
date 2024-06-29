fe4bcec2d10b45fa3d07fb5eac41ca733371ea49
marcodesparza <esparza231093@hotmail.com>
2024-06-28 23:20:34 -0600

    DEL: Unusued function

--- src/projects.ts
+++
@@ -1,7 +0,0 @@
-let projects: string[] = []
-
-projects = [
-  "Jarsa. Mi carrera como administrador", 
-  "ahora vemos que el estar como freelance esta bien",
-  "ser un docente cambio mi forma de persibir a las personas"
-]

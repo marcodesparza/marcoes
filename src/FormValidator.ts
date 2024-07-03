1c56926d0cdaf40ed188e45f37c63e0676ecee5c
marcodesparza <esparza231093@hotmail.com>
2024-07-02 22:37:28 -0600

    Change function

--- src/FormValidator.ts
+++ src/FormValidator.ts
@@ -34,3 +34,24 @@ if (esCorreoElectronicoValido(email)) {
 } else {
   console.log("El correo electrónico no es válido.");
 }
+
+
+function esCorreoElectronicoValido(email) {
+  // Definir el patrón de expresión regular para validar un correo electrónico
+  const patronCorreo =
+    /[a-zA-Z0-9!#$%&'*\/=?^_`\{\|\}~\+\-]([\.]?[a-zA-Z0-9!#$%&'*\/=?^_`\{\|\}~\+\-])+@[a-zA-Z0-9]([^@&%$\/\(\)=?¿!\.,:;]|\d)+[a-zA-Z0-9][\.][a-zA-Z]{2,4}([\.][a-zA-Z]{2})?/;
+
+  // Comprobar si el correo electrónico cumple con el patrón
+  return patronCorreo.test(email);
+}
+
+document
+  .querySelector<HTMLAllCollection>("#submit")
+  .addEventListener("submit", () => {
+    var value1 = document.getElementById("#email").value;
+    if (esCorreoElectronicoValido(value1)) {
+      console.log("El correo electrónico es válido.");
+    } else {
+      console.log("El correo electrónico no es válido.");
+    }
+  });

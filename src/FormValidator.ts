0b1b85e86a5989512bd22b0d71fa1a5276e0d485
marcodesparza <esparza231093@hotmail.com>
2024-07-09 23:46:07 -0600

    test

--- src/FormValidator.ts
+++ src/FormValidator.ts
@@ -55,3 +55,47 @@ document
       console.log("El correo electrónico no es válido.");
     }
   });
+
+  // Ejemplo de uso
+const email = "ejemplo@dominio.com";
+if (esCorreoElectronicoValido(email)) {
+    console.log("El correo electrónico es válido.");
+} else {
+    console.log("El correo electrónico no es válido.");
+}
+document.getElementById('submit').onclick = function() {
+    var value = document.getElementById('email').value;
+    console.log(value)
+    alert(value);
+  }
+
+
+
+  // Ejemplo de uso
+const email = "ejemplo@dominio.com";
+if (esCorreoElectronicoValido(email)) {
+  console.log("El correo electrónico es válido.");
+} else {
+  console.log("El correo electrónico no es válido.");
+}
+
+
+function esCorreoElectronicoValido(email) {
+  // Definir el patrón de expresión regular para validar un correo electrónico
+  const patronCorreo =
+    /[a-zA-Z0-9!#$%&'*\/=?^_`\{\|\}~\+\-]([\.]?[a-zA-Z0-9!#$%&'*\/=?^_`\{\|\}~\+\-])+@[a-zA-Z0-9]([^@&%$\/\(\)=?¿!\.,:;]|\d)+[a-zA-Z0-9][\.][a-zA-Z]{2,4}([\.][a-zA-Z]{2})?/;
+
+  // Comprobar si el correo electrónico cumple con el patrón
+  return patronCorreo.test(email);
+}
+
+document
+  .querySelector<HTMLAllCollection>("#submit")
+  .addEventListener("submit", () => {
+    var value1 = document.getElementById("#email").value;
+    if (esCorreoElectronicoValido(value1)) {
+      console.log("El correo electrónico es válido.");
+    } else {
+      console.log("El correo electrónico no es válido.");
+    }
+  });
